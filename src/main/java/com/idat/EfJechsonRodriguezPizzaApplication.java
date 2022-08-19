9fc61d1c05c92c13e2847fb6a923d9f4f25afd03
unknown <a18101601@idat.edu.pe>
2022-08-19 00:08:51 -0500

    pizza

---
+++ src/main/java/com/idat/EfJechsonRodriguezPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfJechsonRodriguezPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfJechsonRodriguezPizzaApplication.class, args);
+	}
+
+}
